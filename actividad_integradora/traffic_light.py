bb31942f9b42fd46ab290e337fe5fa24142237fb
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2024-11-12 18:15:53 -0600

    Agregado de agente traffic light con timer

--- actividad_integradora/traffic_light.py
+++ actividad_integradora/traffic_light.py
@@ -1,8 +1,15 @@
 import mesa
 
 class Traffic_light(mesa.Agent):
-    def __init__(self, model, unique_id, pos):
+    def __init__(self, model, unique_id, pos, timer_interval=5):
         super().__init__(model)
         self.state = False
-        
+        self.timer = 0
+        self.timer_interval = timer_interval
 
+        def step(self):
+            self.timer += 1
+
+            if self.timer >= self.timer_interval:
+                self.state = not self.state
+                self.timer = 0
