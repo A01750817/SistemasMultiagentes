99cfc18c888f6f1c35111e4c32923cb141cb4d51
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2024-11-13 20:50:34 -0600

    Agregado de edificios

--- actividad_integradora/actividad_model.py
+++ actividad_integradora/actividad_model.py
@@ -41,7 +41,7 @@ class cityClass(mesa.Model):
         self.num_agents = numberAgents
         self.grid = mesa.space.MultiGrid(width, height, torus=False)
         self.schedule = mesa.time.RandomActivation(self)
-        self.edificios = [(2, 3), (2, 4), (2, 5), (3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)]
+        self.edificios = [(2, 3), (2, 4), (2, 5), (3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5), (6, 3), (6, 4), (6, 5)]
         self.create_agents()
         self.create_traffic_lights()
 
--- actividad_integradora/tablero.py
+++ actividad_integradora/tablero.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from actividad_model import cityClass, CarAgent, Traffic_light
 
-edificios = [(2, 3), (2, 4), (2, 5), (3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5)]
+edificios = [(2, 3), (2, 4), (2, 5), (3, 3), (3, 4), (3, 5), (4, 3), (4, 4), (4, 5), (5, 3), (5, 4), (5, 5), (6, 3), (6, 4), (6, 5)]
 
 def plot_grid(model):
     plt.figure(figsize=(8, 8))
