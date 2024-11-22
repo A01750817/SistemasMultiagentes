c0091f4129b0a0a29c62c15b92dab3ea039d3ce2
SantiagoVilla09 <117100375+SantiagoVilla09@users.noreply.github.com>
2024-11-22 10:03:00 -0600

    Agregado de comentarios

--- actividad_integradora/traffic_light.py
+++ actividad_integradora/traffic_light.py
@@ -9,58 +9,37 @@
 # Última modificación: 15/11/2024
 # Fecha de entrega 15/11/2024
 
-# import mesa
-# import asyncio
-# import websockets
-
-# class Traffic_light(mesa.Agent):
-#     """
-#     Clase que modela un agente semáforo dentro de la simulación de la ciudad.
-#     ## Atributos:
-#     - model (cityClass): Referencia al modelo al que pertenece el semáforo.
-#     - unique_id (int): Identificador único del semáforo.
-#     - pos (tuple): Posición del semáforo en la cuadrícula (x, y).
-#     - state (bool): Estado actual del semáforo (`False` para rojo, `True` para verde).
-#     - timer (int): Contador interno que rastrea el tiempo transcurrido desde el último cambio de estado.
-#     - timer_interval (int): Intervalo de tiempo (en pasos de simulación) entre cambios de estado.
-#     ## Métodos:
-#     - __init__(self, model, unique_id, pos, timer_interval=10): Inicializa un semáforo con una posición, un estado inicial (rojo) y un intervalo de tiempo para alternar su estado.
-#     - step(self): Realiza un paso en la simulación. Incrementa el temporizador y alterna el estado del semáforo si el temporizador
-#     alcanza el intervalo configurado.
-#     """
-#     def __init__(self, model, unique_id, pos, timer_interval):
-#         """
-#         Inicializa un semaforo con una posicion, un estado inicial (rojo) y un intervalo de tiempo para alternar su estado.
-#         ## Argumentos:
-#         - model (cityClass): Referencia al modelo al que pertenece el semaforo.
-#         - unique_id (int): Identificador único del semaforo.
-#         - pos (tuple): Posición del semaforo en la cuadrícula (x, y).
-#         - timer_interval (int): Intervalo de tiempo (en pasos de simulación) entre cambios de estado.
-#         """
-#         super().__init__(unique_id, model)
-#         self.pos = pos
-#         self.timer_interval = timer_interval
-#         self.state = True  # True = verde, False = rojo
-#         self.timer = 0
-#         self.type = "traffic_light"
-
-
-#     def step(self):
-#         """
-#         Función que realiza un paso en la simulación. Incrementa el temporizador y alterna el estado del semáforo si el temporizador alcanza el intervalo configurado.
-#         """
-#         self.timer += 1
-#         if self.timer >= self.timer_interval:
-#             self.state = not self.state  # Cambia entre True y False
-#             self.timer = 0  # Reinicia el temporizador
-
 import mesa
+import asyncio
+import websockets
 
 class Traffic_light(mesa.Agent):
     """
     Clase que modela un agente semáforo dentro de la simulación de la ciudad.
+    ## Atributos:
+    - model (cityClass): Referencia al modelo al que pertenece el semáforo.
+    - unique_id (int): Identificador único del semáforo.
+    - pos (tuple): Posición del semáforo en la cuadrícula (x, y).
+    - state (bool): Estado actual del semáforo (`False` para rojo, `True` para verde).
+    - timer (int): Contador interno que rastrea el tiempo transcurrido desde el último cambio de estado.
+    - timer_interval (int): Intervalo de tiempo (en pasos de simulación) entre cambios de estado.
+    ## Métodos:
+    - __init__(self, model, unique_id, pos, timer_interval=10): Inicializa un semáforo con una posición, un estado inicial (rojo) y un intervalo de tiempo para alternar su estado.
+    - step(self): Realiza un paso en la simulación. Incrementa el temporizador y alterna el estado del semáforo si el temporizador
+    alcanza el intervalo configurado.
     """
     def __init__(self, model, unique_id, pos, timer_interval=10):
+        """
+        Inicializa un semaforo con una posicion, un estado inicial (rojo) y un intervalo de tiempo para alternar su estado.
+        ## Argumentos:
+        - model (cityClass): Referencia al modelo al que pertenece el semaforo.
+        - unique_id (int): Identificador único del semaforo.
+        - pos (tuple): Posición del semaforo en la cuadrícula (x, y).
+        - timer_interval (int): Intervalo de tiempo (en pasos de simulación) entre cambios de estado.
+        - self.type crea el agente "traffic_light" como agente, util para identificarlo dentro del modelo
+        - intersection_group agrupa los semáforos que pertenecen a la misma intersección o cuadrante.
+        - self count almacena la cantuidad de coches que detecta el semaforo en frente de su posición
+        """
         super().__init__(unique_id, model)
         self.pos = pos
         self.timer_interval = timer_interval
