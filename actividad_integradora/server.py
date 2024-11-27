2df60d931b71687ebf8567572cfecae7702b44d7
A01749694 <A01749694@tec.mx>
2024-11-27 09:07:52 -0600

    autos en el mapa

--- actividad_integradora/server.py
+++ actividad_integradora/server.py
@@ -11,7 +11,7 @@ city_model = cityClass(numberAgents=num_agents)
 @app.route('/get_coordinates', methods=['GET'])
 def get_coordinates():
     """Devuelve las coordenadas de los agentes al cliente (Unity)."""
-    scale = 2.4  # Ajusta según el cálculo de escala
+    scale = 1.4  # Ajusta según el cálculo de escala
     offset_x = -12  # Offset para centrar el mapa en Unity
     offset_z = 20  # Offset para centrar el mapa en Unity
     road_height = 0.001 # Altura fija para los agentes en Unity 0.001  
@@ -21,9 +21,9 @@ def get_coordinates():
         "car_positions": [
             {
                 "id": agent.unique_id,
-                "x": agent.pos[0],
+                "x": agent.pos[0] * scale,
                 "y": road_height,
-                "z": -agent.pos[1]
+                "z": -agent.pos[1] * scale
             }
             for agent in city_model.schedule.agents if isinstance(agent, CarAgent)
         ]
